9b0207d80313e9931fd3e2a334c0f81c899c6f42
TECHZYBE <160445537+TECHZYBE@users.noreply.github.com>
2024-02-20 08:20:33 +0530

    Delete main directory

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from . import views
-from django.conf.urls.static import static
-from django.conf import settings
-urlpatterns = [
-    path('',views.index,name='index'),
-    path('contact',views.contact,name='contact')
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
